e650012be30e841d027447d853e520e37c58fd2f
tepeed <yozhomie@gmail.com>
2026-09-21 19:13:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 #stripe based chkr  source code
 
 import logging
+import os
 import warnings
 import time
 from telegram.ext import Updater, CommandHandler
@@ -14,8 +15,10 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 #configuration aapna telegram bot token dalna hai yaha
 
+TELEGRAM_TOKEN = os.environ.get("BOT_TOKEN")
+if not TELEGRAM_TOKEN:
+    raise SystemExit("BOT_TOKEN env var not set. export BOT_TOKEN='1234:AAH...' first.")
 
-TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
 STRIPE_API = "https://api.stripe.com/v1/payment_methods"
 SITE_URL_STEP2 = "https://www.act-today.org/wp-admin/admin-ajax.php"
 
